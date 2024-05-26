3245ae4cfc963f30c42c7549df216caf9bbd22cd
Vinicius Coelho <105958570+vCoelh0@users.noreply.github.com>
2024-05-26 12:57:32 -0300

    Login e Cadastro

---
+++ Login e Cadastro/index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const mysql = require('mysql2')
+const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(express.json())
+
+// function conexao(){
+   
+//     mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     database: 'lojaa3',
+//     password:'1234'
+// })
+// }
+
+
+app.post('/cadastro', (req, res)=>{
+    const connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        database: 'lojaa3',
+        password:'1234'
+    })
+
+    const nome = req.body.nome
+    const email = req.body.email
+    const senha = req.body.senha
+
+    console.log(nome)
+    console.log(email)
+    console.log(senha)
+    
+
+    const sql = "INSERT INTO tb_usuario(nome,email, senha ) VALUES('"+nome+"', '"+email+"', '"+senha+"');"
+    connection.query(sql,(err, results)=>{
+        console.log(results)
+        res.send('ok')
+    })
+})
+
+
+app.post('/login', (req,res)=>{
+    const connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        database: 'lojaa3',
+        password:'1234'
+    })
+
+
+
+    const email = req.body.email
+    const senha = req.body.senha
+
+    const sql = "SELECT * FROM tb_usuario WHERE email = ? AND senha = ?";
+    connection.query(sql, [email, senha], (err, results) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Erro ao realizar login');
+        } else {
+            if (results.length > 0) {
+                res.send('Login realizado com sucesso');
+            } else {
+                res.status(401).send('Email ou senha incorretos');
+            }
+        }
+    });
+});
+
+
+
+
+
+app.listen(3000, ()=> console.log('rodando na porta 3000...'))
+
+
+
